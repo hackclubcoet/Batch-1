d84a7893fcb5df7f3eb9368da6a9f49aa8f21bbb
anugrah-a <84310287+anugrah-a@users.noreply.github.com>
2021-07-01 23:05:25 +0530

    Add files via upload

---
+++ Anugrah_A.py
@@ -0,0 +1,10 @@
+# Anugrah A - batch 1 - Reminder Message
+import time
+'''imports time module'''
+# assign 'true' to value
+value = 'true'
+while value:
+    # print TIMES UP after 60 seconds
+    print("TIMES UP")
+    # set time interval of 60 secondsusing sleep() function
+    time.sleep(60)
